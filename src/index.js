0ef73820f1364ab20f9025f9d39baef963b67018
Franklin Borges <franklin.borges-extern@renault.com>
2022-01-25 20:12:57 -0300

    types of parameters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 
+//for get json parameter, use the line below
+app.use(express.json())
+
+// query params: we use like: /courses?page=1&order=asc
 app.get("/courses", (request, response) => {
+  const query = request.query;
+
+  console.log(query)
   return response.json([
     "Curso 1",
     "Curso 2",
@@ -9,7 +16,12 @@ app.get("/courses", (request, response) => {
   ])
 })
 
+
+// body params: we use when we want send a json on body
 app.post("/courses", (request, response) => {
+  const body = request.body
+  console.log(body)
+
   return response.json([
     "Curso 1",
     "Curso 2",
@@ -18,13 +30,12 @@ app.post("/courses", (request, response) => {
   ])
 })
 
+// request params: we use when we need send a ID or something like that on url.
 app.put("/courses/:id", (request, response) => {
-  return response.json([
-    "Curso 5",
-    "Curso 2",
-    "Curso 3",
-    "Curso 4"
-  ])
+  const {id} = request.params;
+
+  console.log(id)
+  return response.json({id: id})
 })
 
 app.patch("/courses/:id", (request, response) => {
