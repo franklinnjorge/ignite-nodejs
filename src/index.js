95d3d7e1f9761c85d26b07cd58267dd5d7a29bb8
Franklin Borges <franklin.borges-extern@renault.com>
2022-01-25 19:52:27 -0300

    create and using http methods (simple way)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({message: "Hello World - yarn add nodemon -D! "})
+app.get("/courses", (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
 })
 
+app.post("/courses", (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.put("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 5",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 5",
+    "Curso 6",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 5",
+    "Curso 6",
+    "Curso 3"
+  ])
+})
 
 app.listen(3333)
 
